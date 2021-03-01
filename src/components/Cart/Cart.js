0ca712721708914799ac4984bc4ab68a3cb69369
Umme Salma Ali <ummes3527@gmail.com>
2021-03-01 18:50:44 +0600

    on-the-way

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,17 +10,25 @@ import './Cart.css'
 const Cart = (props) => {
     const cart = props.cart
     const totalSalary = cart.reduce((total,player) => total +player.salary,0)
-    const addedPlayer = cart.reduce((name,player)=>name+player.name,[])
-    const individualSalary = cart.reduce((name,player) =>name+ player.salary,[])
-    const playerImage = cart.reduce((name,player)=>name+ player.image,[])
+
+    // const individualSalary = cart.reduce((name,player) =>name+ player.salary,[])
+    // const playerImage = cart.reduce((name,player)=>name+ player.image,[])
+    
     return (
         <div className="summary-container">
-            <h3 style={{color:'violet'}}>The Final Squad</h3>
-            <h4><FontAwesomeIcon style={{ color: "green" }} icon={faUserCheck} />Player Added:{cart.length}</h4>
-            <h3>Selected Players:{addedPlayer}</h3>
-           <h4>Salary of Selected Players: {individualSalary}</h4>
-            <h3><FontAwesomeIcon style={{ color: "grey" }} icon={faDollarSign} />Total Budget: ${totalSalary}</h3>
-            <img src={playerImage} alt=""/>
+            <h3 style={{color:'blue'}}>The Final Squad</h3>
+            <h4><FontAwesomeIcon style={{ color: "green" }} icon={faUserCheck} />Player Selected:{cart.length}</h4>
+            {
+                cart.map(player => <h3>Selected Players: {player.name}</h3>)
+            }
+            {
+                cart.map(player => <h3>Individual Salary:{player.salary}</h3>)
+            }
+                        <h3><FontAwesomeIcon style={{ color: "grey" }} icon={faDollarSign} />Total Budget: ${totalSalary}</h3>
+            {
+                cart.map(player => <img src={player.image}></img> )
+            }
+            
             
         </div>
     );
